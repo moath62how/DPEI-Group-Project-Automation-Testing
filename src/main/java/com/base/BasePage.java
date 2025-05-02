69803b25b0d99c24425cb40ffcfbc8602ea34262
moath62how <moath26how@gmail.com>
2025-05-03 01:01:54 +0300

    added base files

--- src/main/java/com/base/BasePage.java
+++ src/main/java/com/base/BasePage.java
@@ -5,9 +5,18 @@ import org.openqa.selenium.*;
 
 public class BasePage {
 	protected WebDriver driver;
-
+	public void setDriver(WebDriver driver){
+		this.driver = driver;
+	}
 	public WebElement find(By locator){
 		return driver.findElement(locator);
 	}
+	protected void set(By locator, String text) {
+		find(locator).clear();
+		find(locator).sendKeys(text);
+	}
 
+	protected void click(By locator) {
+		find(locator).click();
+	}
 }
